ca5599f970765058f3f8f97f356537faeb6aa472
bohdan mykhats <bodiamykhats@gmail.com>
2024-07-21 14:41:08 +0300

    Base architecture

---
+++ GameVault.Server/GameVault.ObjectModel/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameVault.ObjectModel.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Guid { get; set; }
+    }
+}
